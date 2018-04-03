b55d4498b2a87d056c3020726a888b9831f503ed
MoLeighJackson <moleighjackson@gmail.com>
2018-04-03 19:33:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/moleigh/inheritingbehavior/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.moleigh.inheritingbehavior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * deleting onCreate method makes "hello world" disappear
+     *
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
