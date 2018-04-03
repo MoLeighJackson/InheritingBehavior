6d4fad9bec12552f0a126e3b03c1b294bf2a47fb
MoLeighJackson <moleighjackson@gmail.com>
2018-04-03 19:43:26 -0400

    added onCreate method back

--- app/src/main/java/com/example/moleigh/inheritingbehavior/MainActivity.java
+++ app/src/main/java/com/example/moleigh/inheritingbehavior/MainActivity.java
@@ -8,5 +8,10 @@ public class MainActivity extends AppCompatActivity {
      * deleting onCreate method makes "hello world" disappear
      *
      */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
